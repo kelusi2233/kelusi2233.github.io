ccad47fb5d33b0f42d007a029559c51d5f62bcaa
tom <163084651+kelusi2233@users.noreply.github.com>
2024-06-07 20:06:07 +0800

    add

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1717761320';
+const cacheName = 'chirpy-1717761939';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
