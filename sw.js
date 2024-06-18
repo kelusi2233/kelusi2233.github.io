351424e052d753b639ed14fc9392e9fc6edaacdf
kelusi2233 <tom_kelusi2233@proton.me>
2024-06-18 09:19:29 +0000

    Auto deploy

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1718278056';
+const cacheName = 'chirpy-1718702369';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
